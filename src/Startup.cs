5c4394260323b28cb2f78b14264e94c88602c944
Xavier RODET <xavier.rodet@gmail.com>
2021-04-13 13:03:55 +0200

    working again for local dev

--- src/Startup.cs
+++ src/Startup.cs
@@ -31,15 +31,15 @@ namespace SteamOpenIdConnectProvider
 
         public void ConfigureServices(IServiceCollection services)
         {
-
-            // services.Configure<CookiePolicyOptions>(options =>
-            // {
-            //     options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
-            //     options.OnAppendCookie = cookieContext => 
-            //         CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
-            //     options.OnDeleteCookie = cookieContext => 
-            //         CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
-            // });
+            // This fixes old browsers behaviour: https://devblogs.microsoft.com/aspnet/upcoming-samesite-cookie-changes-in-asp-net-and-asp-net-core/
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+                options.OnAppendCookie = cookieContext => 
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+                options.OnDeleteCookie = cookieContext => 
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+            });
 
             services.AddControllers()
                 .AddNewtonsoftJson()
@@ -74,18 +74,11 @@ namespace SteamOpenIdConnectProvider
             services.AddHttpClient<IProfileService, SteamProfileService>();
 
             services.AddAuthentication()
-            .AddCookie(options =>
-            {
-                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-                options.Cookie.SameSite = SameSiteMode.None;
-            })
-            // .AddCookie(new SameSiteCookieManager(new SystemWebCookieManager()))
-            // .AddOpenIdConnect(
-            //     // new OpenIdConnectAuthenticationOptions
-            //     new OpenIdConnectOptions
-            //     {
-            //     // … Your preexisting options … 
-            //     CookieManager = new SameSiteCookieManager(new SystemWebCookieManager())
+            //// Seems useless ? (at leaast for local tests...)
+            // .AddCookie(options =>
+            // {
+            //     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            //     options.Cookie.SameSite = SameSiteMode.None;
             // })
             // TODO: remove AddCookie if confirmed useless
                 // .AddCookie(options =>
@@ -114,36 +107,25 @@ namespace SteamOpenIdConnectProvider
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // Fix the Cookie SameSitePolicy
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
-                // app.UseCookiePolicy(new CookiePolicyOptions
-                // {
-                //     MinimumSameSitePolicy = SameSiteMode.None,
-                //     Secure = CookieSecurePolicy.Always
-                // });
-            } /* else {
-                app.UseCookiePolicy(new CookiePolicyOptions
-                {
-                    MinimumSameSitePolicy = SameSiteMode.Lax,
-                    Secure = CookieSecurePolicy.Always
-                });
-            } */
-
-            // app.UseCookiePolicy();
-            // app.UseOpenIdConnectAuthentication(
-            //     new OpenIdConnectAuthenticationOptions
-            //     {
-            //     // … Your preexisting options … 
-            //     CookieManager = new SameSiteCookieManager(new SystemWebCookieManager())
-            // });
+            }
 
             if (!string.IsNullOrEmpty(Configuration["Hosting:PathBase"]))
             {
                 app.UsePathBase(Configuration["Hosting:PathBase"]);
             }
+
+            // app.UseCookiePolicy();
+
+            // Fix SameSite: https://stackoverflow.com/a/51671538/3254208
+            app.UseCookiePolicy(new CookiePolicyOptions
+            {
+                MinimumSameSitePolicy = SameSiteMode.Lax, 
+                // MinimumSameSitePolicy = SameSiteMode.None,
+                Secure = CookieSecurePolicy.Always
+            });
             
             app.Use(async (ctx, next) =>
             {
@@ -173,28 +155,59 @@ namespace SteamOpenIdConnectProvider
                 endpoints.MapControllers();
                 endpoints.MapHealthChecks("/health");
             });
-
-            // Fix SameSite: https://stackoverflow.com/a/51671538/3254208
-            app.UseCookiePolicy(new CookiePolicyOptions
-            {
-                MinimumSameSitePolicy = SameSiteMode.None,
-                Secure = CookieSecurePolicy.Always
-            });
         }
 
-        // private void CheckSameSite(HttpContext httpContext, CookieOptions options)
-        // {
-        //     if (options.SameSite == SameSiteMode.None)
-        //     {
-        //         var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+        private void CheckSameSite(HttpContext httpContext, CookieOptions options)
+        {
+            if (options.SameSite == SameSiteMode.None)
+            {
+                var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+                // If UserAgent doesn’t support new behavior 
+                if(DisallowsSameSiteNone(userAgent)) {
+                    // options.SameSite = (SameSiteMode)(-1);
+                    options.SameSite = SameSiteMode.Unspecified;
+                }
+            }
+        }
 
-        //         // If UserAgent doesn’t support new behavior 
-        //         if(SameSiteCookieManager.DisallowsSameSiteNone(userAgent)) {
-        //             // options.SameSite = (SameSiteMode)(-1);
-        //             options.SameSite = SameSiteMode.Unspecified;
-        //         }
-        //     }
-        // }
+        private bool DisallowsSameSiteNone(string userAgent)
+        {
+            if (string.IsNullOrEmpty(userAgent))
+            {
+                return false;
+            }
 
+            // Cover all iOS based browsers here. This includes:
+            // - Safari on iOS 12 for iPhone, iPod Touch, iPad
+            // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
+            // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
+            // All of which are broken by SameSite=None, because they use the iOS networking stack
+            if (userAgent.Contains("CPU iPhone OS 12") || userAgent.Contains("iPad; CPU OS 12"))
+            {
+                return true;
+            }
+
+            // Cover Mac OS X based browsers that use the Mac OS networking stack. This includes:
+            // - Safari on Mac OS X.
+            // This does not include:
+            // - Chrome on Mac OS X
+            // Because they do not use the Mac OS networking stack.
+            if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") && 
+                userAgent.Contains("Version/") && userAgent.Contains("Safari"))
+            {
+                return true;
+            }
+
+            // Cover Chrome 50-69, because some versions are broken by SameSite=None, 
+            // and none in this range require it.
+            // Note: this covers some pre-Chromium Edge versions, 
+            // but pre-Chromium Edge does not require SameSite=None.
+            if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
